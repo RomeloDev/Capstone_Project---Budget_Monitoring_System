87d4f24278bad34c8db520a47c54c766dcda53cd
RomeloDev <romelolucot1232@gmail.com>
2025-11-10 18:36:14 +0800

    Prod DATABASE_URL Added

--- bb_budget_monitoring_system/settings.py
+++ bb_budget_monitoring_system/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 import os
 from pathlib import Path
+from dotenv import load_dotenv
+load_dotenv()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -104,16 +107,26 @@ WSGI_APPLICATION = 'bb_budget_monitoring_system.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {   
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres_bb_budget_monitoring_db',
-        'USER': 'postgres',
-        'PASSWORD': 'superuser',
-        'HOST': 'localhost',
-        'PORT': '5432'
-    }
+# This is for local development with PostgreSQL
+# DATABASES = {   
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'postgres_bb_budget_monitoring_db',
+#         'USER': 'postgres',
+#         'PASSWORD': 'superuser',
+#         'HOST': 'localhost',
+#         'PORT': '5432'
+#     }
+# }
+
+
+# This is for production deployment with PostgreSQL (Railway)
+import dj_database_url
+
+DATABASES = {
+    'default': dj_database_url.config(default=os.getenv('DATABASE_URL')),
 }
+
 
 
 # Password validation
